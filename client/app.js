0695072a34a64fcde02712ecacd927c35ff8d27d
AMAN YADAV <amanyadav7964@gmail.com>
2026-04-22 21:26:10 +0530

    Update API URL to production Render link

--- client/app.js
+++ client/app.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://student-login-demo.onrender.com/api';
 
 // Utility for showing alerts
 function showAlert(message, type = 'error') {
